91bc96a6df28d9edec246042f9e288b88ad20c6f
juancamilo399 <juan.angel-h@mail.escuelaing.edu.co>
2020-09-20 11:46:16 -0500

    canvas disponibilidad

--- CINEMA II/src/main/resources/static/js/apimock.js
+++ CINEMA II/src/main/resources/static/js/apimock.js
@@ -1,14 +1,15 @@
 
 apimock = (function () {
 
-    var seats = [[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]];
+    var seats = [[true, true, true, true, true, true, true, false, true, true, true, true], [true, true, true, true, true, true, false, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]];
+    var seats1 = [[false, true, true, true, true, true, true, false, true, true, true, true], [true, true, true, true, true, true, false, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]];
     var mockdata = [];
     var function1Y = {"movie": {"name": "SuperHeroes Movie", "genre": "Action"}, "seats": seats, "date": "2018-12-19 17:00"};
     var function2Y = {"movie": {"name": "The Night", "genre": "Horror"}, "seats": seats, "date": "2018-12-19 19:40"};
     var function3Y = {"movie": {"name": "SuperHeroes Movie", "genre": "Action"}, "seats": seats, "date": "2018-12-19 14:30"};
     var function4Y = {"movie": {"name": "The Enigma", "genre": "Drama"}, "seats": seats, "date": "2018-12-20 17:00"};
 
-    mockdata["cinemaX"] = {"name": "cinemaX", "functions": [{"movie": {"name": "The Enigma", "genre": "Drama"}, "seats": seats, "date": "2018-12-18 15:30"}, {"movie": {"name": "The Enigma 2", "genre": "Drama"}, "seats": seats, "date": "2018-12-18 15:30"}]};
+    mockdata["cinemaX"] = {"name": "cinemaX", "functions": [{"movie": {"name": "The Enigma", "genre": "Drama"}, "seats": seats1, "date": "2018-12-18 15:30"}, {"movie": {"name": "The Enigma 2", "genre": "Drama"}, "seats": seats, "date": "2018-12-18 15:30"}]};
     mockdata["cinemaY"] = {"name": "cinemaY", "functions": [function1Y, function2Y, function3Y, function4Y]};
     mockdata["cinemaA"] = {"name": "cinemaA", "functions": [function1Y, function2Y]};
     mockdata["cinemaB"] = {"name": "cinemaB", "functions": [function3Y, function2Y , function1Y]};
@@ -26,7 +27,15 @@ apimock = (function () {
                 mockdata[cinema_name].functions.filter(
                     funct => funct.date.includes(fdate))
             );
+        },
+
+        getFunctionByNameAndDate: function (cinema_name, fdate, movie_name, callback) {
+            var data = mockdata[cinema_name].functions.filter(
+                funct => funct.date.includes(fdate));
+            var funcion = data.find(f => f.movie.name == movie_name);
+            callback(funcion);
         }
+
 
     }
 
--- CINEMA II/src/main/resources/static/js/app.js
+++ CINEMA II/src/main/resources/static/js/app.js
@@ -9,29 +9,78 @@ var Module = (function () {
 				hora:cinemaFunction.date.split(" ")[1]
 			}
 		})
-	};
+	}
+
 	function _table(cinemaFunctions){
 		functions = _map(cinemaFunctions);
 		$("#table_cinema > tbody").empty();
 		functions.map(function(f){
+			var onclick = "Module.getAvailability(\""+f.name+"\")";
+			var stri="'"+onclick+"'";
+			var boton = "<input type='button' class='btn btn-primary' value='Open Seats' onclick=" + stri + "></input>";
+			console.log(boton);
 			$("#table_cinema > tbody").append(
 				"<tr>" +
 					"<td>" + f.name + "</td>"+
 					"<td>" + f.genero + "</td>"+
 					"<td>" + f.hora + "</td>"+
+					"<td>" + boton + "</td>"+
+
 				"</tr>"
 			);
 		});
-	};
+	}
+
+	function getAvailability(movieName) {
+		console.log(movieName)
+		cine = $("#name_input").val();
+		date = $("#date_input").val();
+		$("#movie_name").text("Availability of: "+movieName);
+		apimock.getFunctionByNameAndDate(cine,date,movieName,drawCanvas);
+
+	}
+
+
+	function drawCanvas(data) {
+		clearCanvas();
+		var asientos = data.seats;
+		var c = document.getElementById("canvasId");
+		var ctx = c.getContext("2d");
+
+		ctx.fillStyle = "#0531ae";
+		ctx.fillRect(60, 20 , 380, 40);
+		ctx.beginPath();
+		var a = document.getElementById("canvasId");
+
+		var atx = a.getContext("2d");
+		for (var i = 0; i < asientos[0].length; i++) {
+			for (var j = 0; j < asientos.length; j++) {
+				atx.fillStyle = "#9d9a9a";
+				if(asientos[j][i] == false){
+					atx.fillStyle = "#b00303";
+				}
+				atx.fillRect(i*40 + 10, j*40 +105 , 20, 20);
+			}
+		}
+	}
+
+	function clearCanvas(){
+		var canvas = document.getElementById("canvasId");
+		var ctx = canvas.getContext("2d");
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.beginPath();
+	}
+
 
 	function getCinemaFunctions(){
 		cinemaName = $("#name_input").val();
 		cinemaDate = $("#date_input").val();
 		$("#cinema_name").text("Cinema name : "+ cinemaName);
 		apimock.getFunctionsByCinemaAndDate(cinemaName,cinemaDate,_table)
-	};
+	}
 
 	return {
-		getCinemaFunctions: getCinemaFunctions
+		getCinemaFunctions: getCinemaFunctions,
+		getAvailability: getAvailability
 	};
 })();
